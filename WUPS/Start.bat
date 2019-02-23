4a4853ca7102345c9518baf007e0f7804a2d703c
sori883 <service@dlme.biz>
2019-02-24 05:46:00 +0900

    ダウンロード利用を想定してUnblock-Fileを追加しました

--- WUPS/Start.bat
+++ WUPS/Start.bat
@@ -1,7 +1,9 @@
 @echo off
 
+SET Root=%~dp0
 
-cd /d %~dp0
+cd /d %Root%
+powershell -Command "Get-ChildItem %Root% -Recurse | Unblock-File"
 powershell -Command "Set-ExecutionPolicy RemoteSigned"
 powershell -Command ".\Install-WindowsUpdate.ps1"
 powershell -Command "Set-ExecutionPolicy Restricted"
